8ee29e15c0f80ceb66d5e2ab80c6602cd5a7f4fa
github username <github email address>
2018-11-23 20:07:08 -0500

    updating

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,29 @@
-# your code goes here
+class Peson
+
+
+  attr_accessor :bank_account, :nappiness_points, :hygine_points
+  attr_reader :name
+
+   def initialize()
+      @name = name
+      @bank_account = bank_account = 25
+      @happiness_points = happiness_points = 8
+      @hygine_points = hygine_points = 8
+   end
+
+def happyness(number)
+  hapiness_points = hapiness_points + number
+end
+
+def hygine(num)
+  hygine_points = hygine_points + num
+end
+
+  def get_paid(salary)
+    bank_account = bank_account + salary
+
+take a bath
+call a friend
+start a conversation
+state if they are happy and/or clean
+end
